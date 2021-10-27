aa9a380fe66fe5db516347b0ed90bd5251f1f23b
Daniel <danielelielgaio@gmail.com>
2021-10-27 07:34:53 -0300

    Terminada aula de ifs

---
+++ 5-condicionais.js
@@ -0,0 +1,26 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+  `Salvador`,
+  `São Paulo`,
+  `Rio de Janeiro`
+);
+
+const idadeComprador = 16;
+console.log("Lista de destinos possíveis: ");
+console.log(listaDeDestinos);
+
+if(idadeComprador >= 18){
+  console.log("Comprador maior de idade")
+  listaDeDestinos.splice(1,1);
+}else{
+  console.log("Comprador não é maior de idade e não posso vender")
+}
+
+console.log(listaDeDestinos);
+
+// logic operators
+console.log(idadeComprador > 18);
+console.log(idadeComprador < 18);
+console.log(idadeComprador >= 18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador == 18);

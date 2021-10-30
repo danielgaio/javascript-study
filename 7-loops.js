a7c1ec343ecdf21e115b5865aeab61904df953e6
Daniel <danielelielgaio@gmail.com>
2021-10-30 17:51:06 -0300

    Estudado o for e o debuger do VS Code

--- 7-loops.js
+++ 7-loops.js
@@ -8,7 +8,8 @@ const listaDeDestinos = new Array(
 const idadeComprador = 19;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
-const destino = "São Paulo";
+// const destino = "São Paulo";
+const destino = "Curitiba";
 
 console.log("\nLista de destinos possíveis: ");
 console.log(listaDeDestinos);
@@ -17,16 +18,25 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada
 
 let contador = 0;
+let destinoExiste = false;
 while (contador < 3) {
   if (listaDeDestinos[contador] == destino) {
-    console.log("Destino existente");
-  } else {
-    console.log("Destino não existente");
+    destinoExiste = true;
+    break;
   }
   contador++;
 }
 
+console.log("Destino existe: ", destinoExiste);
 
-while(false){
-    console.log(1);
+if (podeComprar && destinoExiste) {
+  console.log("Boa viagem");
+} else {
+  console.log("Desculpe, tivemos um erro.");
+}
+
+for (let i = 0; i < 3; i++) {
+  if (listaDeDestinos[i] == destino) {
+    destinoExiste = true;
+  }
 }

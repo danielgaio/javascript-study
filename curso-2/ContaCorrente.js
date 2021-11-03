f1c68115cd82998e7679f65ff51201f68e5ea513
Daniel <danielelielgaio@gmail.com>
2021-11-03 07:30:17 -0300

    Trabalhando com objetos compostos

--- curso-2/ContaCorrente.js
+++ curso-2/ContaCorrente.js
@@ -3,6 +3,7 @@ export class ContaCorrente{
   // até esse momento o # ainda não está implementado e o padrão da comunidade
   // é usar um '_' no começo pra sinalizar que ela é privada. 
   #saldo = 0;
+  cliente;
 
   sacar(valor) {
     if (this.#saldo >= valor) {
@@ -18,9 +19,19 @@ export class ContaCorrente{
     if (valor <= 0) return "Erro: Deposito de valor zero ou negativo";
 
     this.#saldo += valor;
-    // console.log(this.#saldo);
+    console.log("Depósito de %s para %s", valor, this.cliente.nome);
   }
+
   exibirSaldo() {
-    console.log(this.#saldo);
+    console.log("Saldo atual de %s: %s",this.cliente.nome, this.#saldo);
+  }
+
+  // variáveis primitivas (valor) chegam na função como cópias, enquanto que
+  // em objetos (conta) chega o original. Então alterar o objeto neste escopo
+  // vai altera-lo no escopo externo, já com a variável primitiva isso não
+  // acontece. Objetos são passados por referência.
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
   }
 }
--- curso-2/index.js
+++ curso-2/index.js
@@ -1,30 +1,50 @@
 import { Cliente } from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
+// instanciar novos clientes
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 cliente1.rg = 123456789;
+console.log(cliente1);
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88855533309;
-console.log(cliente1);
+cliente2.rg = 100457689;
 console.log(cliente2);
 
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-console.log(contaCorrenteRicardo);
+// instanciando novas contas
+const conta1 = new ContaCorrente();
+conta1.agencia = 1001;
+conta1.cliente = cliente1;
+console.log(conta1);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+console.log(conta2);
 
 // faz deposito
-contaCorrenteRicardo.depositar(100);
-console.log(contaCorrenteRicardo.depositar(-10));
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+console.log("\n");
+conta1.depositar(100);
+console.log(conta1.depositar(-10));
+conta1.depositar(100);
 
 // faz saque
-contaCorrenteRicardo.exibirSaldo();
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
-contaCorrenteRicardo.exibirSaldo();
+console.log("\n");
+conta1.exibirSaldo();
+const valorSacado = conta1.sacar(50);
+console.log("Valor sacado de %s: %s",conta1.cliente.nome ,valorSacado);
+conta1.exibirSaldo();
 
+// transferir valor
+console.log("\n");
+conta1.exibirSaldo();
+conta2.exibirSaldo();
+conta1.transferir(50, conta2);
+console.log(
+  "Realizada transferência de %s para %s",
+  conta1.cliente.nome,
+  conta2.cliente.nome
+);
+conta2.exibirSaldo();
+conta1.exibirSaldo();

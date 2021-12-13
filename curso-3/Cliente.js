134c21124229a93f1efc9c5d7af7b541234f522a
Daniel <danielelielgaio@gmail.com>
2021-12-12 22:53:35 -0300

    Adicionando autenticação por senha
    - Utilizando método abstrato

--- curso-3/Cliente.js
+++ curso-3/Cliente.js
@@ -1,8 +1,9 @@
 export class Cliente{
-  constructor(nome, cpf, rg) {
+  constructor(nome, cpf, rg, senha) {
     this.nome = nome;
     this._cpf = cpf;
     this._rg = rg;
+    this._senha = senha;
   }
 
   get cpf() { return this._cpf; }
--- curso-3/index.js
+++ curso-3/index.js
@@ -1,35 +1,56 @@
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from ".//Conta/Corrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+// import { Cliente } from "./Cliente.js";
+// import { ContaCorrente } from ".//Conta/Corrente.js";
+// import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+// import { ContaSalario } from "./Conta/ContaSalario.js";
 
-// instanciar novos clientes
-const cliente1 = new Cliente("Ricardo", 11122233309, 123456789);
+// // instanciar novos clientes
+// const cliente1 = new Cliente("Ricardo", 11122233309, 123456789);
 
-// instanciando novas contas
-const contaCorrente1 = new ContaCorrente(cliente1, 1001);
+// // instanciando novas contas
+// const contaCorrente1 = new ContaCorrente(cliente1, 1001);
 
-// faz deposito
-contaCorrente1.depositar(100);
-console.log(contaCorrente1.depositar(-10));
-contaCorrente1.depositar(100);
+// // faz deposito
+// contaCorrente1.depositar(100);
+// console.log(contaCorrente1.depositar(-10));
+// contaCorrente1.depositar(100);
 
-// faz saque
-console.log("\n");
-contaCorrente1.exibirSaldo();
-const valorSacado = contaCorrente1.sacar(50);
-contaCorrente1.sacar(50);
-// console.log("Valor sacado de %s: %s", contaCorrente1.cliente.nome, valorSacado);
+// // faz saque
+// console.log("\n");
 // contaCorrente1.exibirSaldo();
-// console.log("Saldo atual de %s: %s", contaCorrente1.cliente.nome, contaCorrente1.saldo);
+// const valorSacado = contaCorrente1.sacar(50);
+// contaCorrente1.sacar(50);
+// // console.log("Valor sacado de %s: %s", contaCorrente1.cliente.nome, valorSacado);
+// // contaCorrente1.exibirSaldo();
+// // console.log("Saldo atual de %s: %s", contaCorrente1.cliente.nome, contaCorrente1.saldo);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
+// const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+// contaPoupanca.sacar(10);
 
-// console.log(contaPoupanca);
-// console.log(contaCorrente1);
+// // console.log(contaPoupanca);
+// // console.log(contaCorrente1);
 
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-console.log(contaSalario);
+// const contaSalario = new ContaSalario(cliente1);
+// contaSalario.depositar(100);
+// contaSalario.sacar(10);
+// console.log(contaSalario);
+
+import { Cliente } from "./Cliente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+
+// No vs code selecionar paravra e pressionar ctrl+d para achar próxima referência e editar todas ao mesmo tempo
+
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ricardo", 5000, 12345633445);
+gerente.cadastrarSenha("123");
+
+const cliente = new Cliente("Lais", 24743468345, 3456643223, "12345");
+// a função de login é polimórfica
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+console.log(gerenteEstaLogado);
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "12345");
+

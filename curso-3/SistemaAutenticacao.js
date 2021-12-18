d491caa21bb128e453630834d3ffe15ca60e304a
Daniel <danielelielgaio@gmail.com>
2021-12-18 11:38:52 -0300

    Update SistemaAutenticacao.js
    - Aprendendo o conceito de duck type

--- curso-3/SistemaAutenticacao.js
+++ curso-3/SistemaAutenticacao.js
@@ -1,5 +1,11 @@
 /**
  * Ser autenticavel significa ter o método autenticar
+ * ducky type (o autenticavel da função login()). Comum em linguagem fracamente
+ * tipadas.
+ * Definition: duck typing is a style of dynamic typing in which an object's
+ * current set of methods and properties determines the valid semantics, rather
+ * than its inheritance from a particular class or implementation of a specific
+ * interface.
  */
 
 export class SistemaAutenticacao{

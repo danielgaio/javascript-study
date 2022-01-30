7b1e4abdb845251ab153b9bb19cd7ec2b4d38a5e
Daniel <danielelielgaio@gmail.com>
2022-01-29 21:12:33 -0300

    Adicionado Stepper ao andamento do formulário

--- curso-7/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ curso-7/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,23 +1,49 @@
-import React, { useState } from "react";
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 // As chaves servem para receber a propriedade de modo desconstruido
-function FormularioCadastro({aoEnviar, validarCpf}) {
+function FormularioCadastro({ aoEnviar, validarCpf }) {
+  // Estados
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState({});
+  // Efeitos
+  useEffect(() => {
+    if (etapaAtual === formularios.length-1) {
+      aoEnviar(dadosColetados);
+    }
+  });
   
   const formularios = [
-    <DadosUsuario aoEnviar={proximo} />,
-    <DadosPessoais aoEnviar={proximo} validarCpf={validarCpf} />,
-    <DadosEntrega aoEnviar={aoEnviar} />
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCpf={validarCpf} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
+    <Typography variant="h5">Obrigado pelo cadastro!</Typography>
   ];
 
+  function coletarDados(dados) {
+    // spread operator vai inserir os dados do vetor um a um e não o vetor todo como um elemento único
+    setDados({ ...dadosColetados, ...dados });
+    proximo();
+  }
+  
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  return <> { formularios[etapaAtual] } </>;
+  return (
+    <>
+      <Stepper activeStep={etapaAtual}>
+        <Step><StepLabel>Login</StepLabel></Step>
+        <Step><StepLabel>Dados pessoais</StepLabel></Step>
+        <Step><StepLabel>Entrega</StepLabel></Step>
+        <Step><StepLabel>Finalização</StepLabel></Step>
+      </Stepper>
+      {formularios[etapaAtual]}
+    </>
+  );
 }
 
 export default FormularioCadastro;

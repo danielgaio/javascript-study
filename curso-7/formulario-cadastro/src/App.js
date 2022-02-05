ce7e540dc4fb7aa085163e3ce3f313d77e1ff5f0
Daniel <danielelielgaio@gmail.com>
2022-02-05 12:43:27 -0300

    Utilizando valores default no provider

--- curso-7/formulario-cadastro/src/App.js
+++ curso-7/formulario-cadastro/src/App.js
@@ -7,23 +7,20 @@ import ValidacoesCadastro from "./contexts/ValidacoesCadastro";
 import { validarCpf, validarSenha } from "./models/cadastro";
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <Container component="article" maxWidth="sm">
-        <Typography
-          variant="h3"
-          component="h1"
-          align="center"
-        >
+        <Typography variant="h3" component="h1" align="center">
           Formulário de cadastro
         </Typography>
-        // criação de um contexto
-        <ValidacoesCadastro.Provider value={{
-          cpf: validarCpf,
-          senha: validarSenha,
-          nome: validarSenha
-        }}>
-          <FormularioCadastro aoEnviar={aoEnviarForm}/>
+        <ValidacoesCadastro.Provider
+          value={{
+            cpf: validarCpf,
+            senha: validarSenha,
+            nome: validarSenha,
+          }}
+        >
+          <FormularioCadastro aoEnviar={aoEnviarForm} />
         </ValidacoesCadastro.Provider>
       </Container>
     );

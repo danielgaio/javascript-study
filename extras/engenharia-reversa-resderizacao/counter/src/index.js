6b12e42e153f1b47ba61ad38a059ae467f84ba61
Daniel <danielelielgaio@gmail.com>
2022-02-06 10:34:17 -0300

    Iniciando engenharia reversa da resderização

---
+++ extras/engenharia-reversa-resderizacao/counter/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {render} from "react-dom";
+
+function App() {
+  return (
+    <section className="App">
+      <h1>Contador</h1>
+      <div>
+        <div>0</div>
+        <button>Incrementar</button>
+        <button>Decrementar</button>
+      </div>
+    </section>
+  );
+}
+
+render(<App />, document.querySelector('#root'));

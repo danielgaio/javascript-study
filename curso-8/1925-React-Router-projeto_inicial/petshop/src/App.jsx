f84387b98cf9fe6717ba326430c9ce12eaa6b60c
Daniel Gaio <danielelielgaio@gmail.com>
2022-02-19 17:09:44 -0300

    Criando cabeçalho

--- curso-8/1925-React-Router-projeto_inicial/petshop/src/App.jsx
+++ curso-8/1925-React-Router-projeto_inicial/petshop/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/css/base/base.css";
+import Cabecalho from "./components/Cabecalho";
 import Home from "./paginas/Home";
-import Sobre from "./paginas/Sobre";
 import Pagina404 from "./paginas/Pagina404";
+import Sobre from "./paginas/Sobre";
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path="/">
           <Home />
@@ -16,7 +18,7 @@ function App() {
           <Sobre />
         </Route>
         <Route>
-          <Pagina404/>
+          <Pagina404 />
         </Route>
       </Switch>
     </Router>

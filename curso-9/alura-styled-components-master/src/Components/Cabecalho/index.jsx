39c74f8158bdf5107f9e9c163afd02ae08c4c6b8
Daniel <danielelielgaio@gmail.com>
2022-04-28 22:44:32 -0300

    Ajuste da importação de fonte por link. Styled component para estilo global. Refatoração do cabeçalho para styled component. Criação de módulo para armazenar variáveis.

--- curso-9/alura-styled-components-master/src/Components/Cabecalho/index.jsx
+++ curso-9/alura-styled-components-master/src/Components/Cabecalho/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import { corPrimaria } from "../UI/variaveis";
+
+const StyledHeader = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
+    <StyledHeader>
       <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
       <div>
         <a className="btn-secundario" href="https://google.com">
@@ -13,7 +24,7 @@ const Cabecalho = () => {
           Sair
         </a>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 

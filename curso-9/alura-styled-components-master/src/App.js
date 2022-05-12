ec6e2886ea8bc7052bdf03244d0376609d993e18
Daniel <danielelielgaio@gmail.com>
2022-05-11 23:30:04 -0300

    - Realizada a configuração para suporte e troca entre tema claro e escuro.

--- curso-9/alura-styled-components-master/src/App.js
+++ curso-9/alura-styled-components-master/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaEscuro}>
       <GlobalStyle />
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 

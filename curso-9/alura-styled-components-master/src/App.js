5f686ae11c37fbb3b46e1c9cc09f472bae67fef6
Daniel <danielelielgaio@gmail.com>
2022-06-07 22:26:58 -0300

    Projeto e curso finalizado - Adicionado gif de demonstração do projeto. - Finalizada a funcionalidade de troca de tema.

--- curso-9/alura-styled-components-master/src/App.js
+++ curso-9/alura-styled-components-master/src/App.js
@@ -4,13 +4,19 @@ import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
 import { temaClaro, temaEscuro } from "./Components/UI/temas";
+import { BtnTema } from './Components/UI'
+import SwitcherTema from './Components/SwitcherTema'
 
 function App() {
+  const [tema, setTema] = useState(true)
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  }
   return (
-    <ThemeProvider theme={temaEscuro}>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
-      <BtnTema>
-        <SwitcherTema />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={tema}/>
       </BtnTema>
       <Cabecalho />
       <Container />
